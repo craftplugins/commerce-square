30c4163473fed4614a50f81f87e3056980423afa
Joshua Baker <160484+joshuabaker@users.noreply.github.com>
2020-07-14 19:45:49 +0100

    Reformated file
    Signed-off-by: Joshua Baker <160484+joshuabaker@users.noreply.github.com>

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -15,6 +15,21 @@ class Install extends Migration
     /**
      * @return bool
      */
+    public function safeDown(): bool
+    {
+        MigrationHelper::dropAllForeignKeysOnTable(
+            '{{%square_customers}}',
+            $this
+        );
+
+        $this->dropTable('{{%square_customers}}');
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
     public function safeUp(): bool
     {
         $this->createTable('{{%square_customers}}', [
@@ -28,19 +43,25 @@ class Install extends Migration
             'uid' => $this->uid(),
         ]);
 
-        $this->addForeignKey(null, '{{%square_customers}}', 'gatewayId', '{{%commerce_gateways}}', 'id', 'CASCADE', null);
-        $this->addForeignKey(null, '{{%square_customers}}', 'userId', '{{%users}}', 'id', 'CASCADE', null);
+        $this->addForeignKey(
+            null,
+            '{{%square_customers}}',
+            'gatewayId',
+            '{{%commerce_gateways}}',
+            'id',
+            'CASCADE',
+            null
+        );
 
-        return true;
-    }
-
-    /**
-     * @return bool
-     */
-    public function safeDown(): bool
-    {
-        MigrationHelper::dropAllForeignKeysOnTable('{{%square_customers}}', $this);
-        $this->dropTable('{{%square_customers}}');
+        $this->addForeignKey(
+            null,
+            '{{%square_customers}}',
+            'userId',
+            '{{%users}}',
+            'id',
+            'CASCADE',
+            null
+        );
 
         return true;
     }
